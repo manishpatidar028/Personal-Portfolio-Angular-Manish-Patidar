a7af75ddc736bcda3e0e6aa5d28db7d32a59e708
manishpatidar028 <52074428+manishpatidar028@users.noreply.github.com>
2020-11-02 19:30:57 -0800

    CV link updated

--- src/app/modules/portfolio-components/components/about/about.component.ts
+++ src/app/modules/portfolio-components/components/about/about.component.ts
@@ -13,7 +13,7 @@ export class AboutComponent implements OnInit {
   }
 
   downloadCV() {
-    window.open('https://docs.google.com/document/d/18wtWpzwCfdf3C-hn3Rn_FeWJnGcx-ev0064RvsMl3T0/edit?usp=drivesdk', '_blank');
+    window.open('https://drive.google.com/file/d/1vZ5lKucfaUZjFs1xN0iuA8I81q2grGXa/view?usp=sharing', '_blank');
   }
 
 }
